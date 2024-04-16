af58d51d30d477c50f44e272f201d5a31fed6a9a
Nihal <nihalbarhaiyya@gmail.com>
2024-04-16 16:41:20 +0530

    day-6

--- Intro_java/Practice.java
+++ Intro_java/Practice.java
@@ -1,5 +1,8 @@
 public class Practice{
     public static void main(String[] args) {
-        System.out.println("hello");
+        int n = 4;
+        int m = 20;
+        System.out.println(m+n);
+
     }
 }
